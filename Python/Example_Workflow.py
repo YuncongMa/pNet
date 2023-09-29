3fe9cc37cdb15f6ab4fb9e65a8ebfad236fc8c11
Yuncong Ma <ma19880608@gmail.com>
2023-09-29 09:53:12 -0400

    Add files via upload

--- Python/Example_Workflow.py
+++ Python/Example_Workflow.py
@@ -1,4 +1,4 @@
-# Yuncong Ma, 9/28/2023
+# Yuncong Ma, 9/29/2023
 # Examples of setting up a workflow of pNet
 # Those examples provide minimal settings to run a workflow
 # Each example has a brief description
@@ -18,13 +18,13 @@ if Example == 1:
     # 4. Choose the desired number of FNs
 
     # Setup
-    dir_pnet_result = '/Volumes/Scratch_0/pNet/Test/Test_FN17_HCP_Workflow'
+    dir_pnet_result = '<User>/Test_FN17_HCP_Workflow'  # Change <User> to a desired directory
     file_scan = '/Volumes/Scratch_0/pNet/Example/HCP_Surface/Data/Scan_List.txt'
     file_Brain_Template = pNet.Brain_Template.file_HCP_surf
     K = 17
 
     # Run pNet workflow
-    pNet.run_workflow_simple(
+    pNet.workflow_simple(
         dir_pnet_result=dir_pnet_result,
         dataType='Surface',
         dataFormat='HCP Surface (*.cifti, *.mat)',
@@ -41,13 +41,13 @@ elif Example == 2:
     # 4. Choose the desired number of FNs
 
     # Setup
-    dir_pnet_result = '/Volumes/Scratch_0/pNet/Test/Test_FN17_UKBB_Workflow'
+    dir_pnet_result = '<User>/Test_FN17_UKBB_Workflow'   # Change <User> to a desired directory
     file_scan = '/Volumes/Scratch_0/pNet/Test/Test_FN17_UKBB/Data_Input/Scan_List.txt'
     file_Brain_Template = pNet.Brain_Template.file_MNI_vol
     K = 17
 
     # Run pNet workflow
-    pNet.run_workflow_simple(
+    pNet.workflow_simple(
         dir_pnet_result=dir_pnet_result,
         dataType='Volume',
         dataFormat='Volume (*.nii, *.nii.gz, *.mat)',
