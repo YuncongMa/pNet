323695e59a6336e2de7406fd88c2a45256c6a737
Yuncong Ma <ma19880608@gmail.com>
2024-02-13 12:15:36 -0500

    change to pnet

---
+++ Python/pnet.py
@@ -0,0 +1,36 @@
+# Yuncong Ma, 1/19/2024
+# pNet
+# This script provides the highest level organization of pNet
+# It provides workflows of pNet, and examples
+# It includes five modules of pNet, underlying functions
+
+#########################################
+# Packages
+import os
+
+# path of pNet
+current_file_path = os.path.abspath(__file__)
+dir_python = os.path.dirname(current_file_path)
+dir_pNet = os.path.dirname(dir_python)
+dir_brain_template = os.path.join(dir_pNet, 'Brain_Template')
+dir_example = os.path.join(dir_pNet, 'Example')
+
+
+# Example
+
+# Brain templates
+
+# Module
+# This script builds the five modules of pNet
+# Functions for modules of pNet
+from Module.Data_Input import *
+from Module.FN_Computation_torch import *
+from Module.FN_Computation import *
+from Module.Visualization import *
+from Module.Quality_Control import *
+from Module.FN_Computation_torch import *
+from Report.Web_Report import *
+from Workflow.Workflow import *
+
+
+
